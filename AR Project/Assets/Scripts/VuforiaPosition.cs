c6bb29587e03acde65cc9532ea90595b291fad44
elliotjb <elliot.jimenez1@gmail.com>
2018-05-13 10:37:14 +0200

    Fixed scene jordi

--- AR Project/Assets/Scripts/VuforiaPosition.cs
+++ AR Project/Assets/Scripts/VuforiaPosition.cs
@@ -16,9 +16,6 @@ public class VuforiaPosition : MonoBehaviour {
         Vector3 pos = transform.position;
 
         child.transform.position = new Vector3(pos.x, child.transform.position.y, pos.z);
-        //child.transform.rotation = transform.rotation;
-        //child.transform.RotateAround(Vector3.zero, Vector3.up, transform.rotation.eulerAngles.y);
-        //child.transform.rotation.SetLookRotation(transform.rotation.eulerAngles, Vector3.up);
         child.transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.eulerAngles.y, 0));
 
 
